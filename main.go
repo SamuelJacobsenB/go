8c3bbbeac0a4d930e25053fc9c9d0b2a77523ca6
Samuel Jacobsen Barcellos <samueljb.dev@gmail.com>
2025-07-05 21:38:51 -0300

    Aprendendo sobre tipos e declaração de variáveis e constantes em Go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var teste string
+	teste = "Este é um teste"
+	fmt.Println(teste)
+
+	var autoTyped = "Hello"
+	fmt.Println(autoTyped)
+
+	var a, b int = 1, 2
+	fmt.Println(a, b)
+
+	variable := true
+	fmt.Println(variable)
+
+	const constante = 10
+}
